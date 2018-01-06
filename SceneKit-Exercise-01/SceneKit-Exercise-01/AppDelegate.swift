3edfd69263ec79f1943201a5348f18263358b571
Teemu Harju <teemu.harju@gmail.com>
2018-01-06 11:46:02 +0200

    Added base for the SceneKit-Exercise-01.

---
+++ SceneKit-Exercise-01/SceneKit-Exercise-01/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SceneKit-Exercise-01
+//
+//  Created by Teemu Harju on 06/01/2018.
+//  Copyright © 2018 Shipyard Games Oy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ SceneKit-Exercise-01/SceneKit-Exercise-01/GameViewController.swift
@@ -0,0 +1,68 @@
+//
+//  GameViewController.swift
+//  SceneKit-Exercise-01
+//
+//  Created by Teemu Harju on 06/01/2018.
+//  Copyright © 2018 Shipyard Games Oy. All rights reserved.
+//
+
+import SceneKit
+import QuartzCore
+
+class GameViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/Scene.scn")!
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // Add a click gesture recognizer
+        let clickGesture = NSClickGestureRecognizer(target: self, action: #selector(handleClick(_:)))
+        var gestureRecognizers = scnView.gestureRecognizers
+        gestureRecognizers.insert(clickGesture, at: 0)
+        scnView.gestureRecognizers = gestureRecognizers
+    }
+    
+    @objc
+    func handleClick(_ gestureRecognizer: NSGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are clicked
+        let p = gestureRecognizer.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = NSColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = NSColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+}
